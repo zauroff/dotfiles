b08e51b2a15fb5c771389d80f3a04075c8a26b2a
zauroff <dannyzny@gmail.com>
2026-03-02 22:39:23 -0500

    added better plugin for tabs

--- wezterm.lua
+++ wezterm.lua
@@ -6,22 +6,29 @@ local config = wezterm.config_builder()
 
 local act = wezterm.action
 
-config.keys = {-- Split pane vertically (creates a pane to the right)
-  { key = "s",          mods = "CTRL|ALT", action = act.SplitPane({ direction = "Right" }) },
-  { key = "Enter",      mods = "SHIFT",    action = wezterm.action.SendString("\x1b\r") },
+-- tab bar plugin
+local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
+bar.apply_to_config(config)
 
-  -- Split pane horizontally (creates a pane below)
-  { key = "v",          mods = "CTRL|ALT", action = act.SplitPane({ direction = "Down" }) },
+config.keys = { -- Split pane vertically (creates a pane to the right)
+	{ key = "s", mods = "CTRL|ALT", action = act.SplitPane({ direction = "Right" }) },
+	{ key = "Enter", mods = "SHIFT", action = wezterm.action.SendString("\x1b\r") },
 
-  -- Close current pane
-  { key = "w",          mods = "CTRL|ALT", action = act.CloseCurrentPane({ confirm = false }) },
+	-- Split pane horizontally (creates a pane below)
+	{ key = "v", mods = "CTRL|ALT", action = act.SplitPane({ direction = "Down" }) },
 
+	-- Close current pane
+	{ key = "w", mods = "CTRL|ALT", action = act.CloseCurrentPane({ confirm = false }) },
 
-  -- Navigate between panes
-  { key = "LeftArrow",  mods = "CTRL|ALT", action = act.ActivatePaneDirection("Left") },
-  { key = "RightArrow", mods = "CTRL|ALT", action = act.ActivatePaneDirection("Right") },
-  { key = "UpArrow",    mods = "CTRL|ALT", action = act.ActivatePaneDirection("Up") },
-  { key = "DownArrow",  mods = "CTRL|ALT", action = act.ActivatePaneDirection("Down") },
+	-- Navigate between panes
+	{ key = "LeftArrow", mods = "CTRL|ALT", action = act.ActivatePaneDirection("Left") },
+	{ key = "RightArrow", mods = "CTRL|ALT", action = act.ActivatePaneDirection("Right") },
+	{ key = "UpArrow", mods = "CTRL|ALT", action = act.ActivatePaneDirection("Up") },
+	{ key = "DownArrow", mods = "CTRL|ALT", action = act.ActivatePaneDirection("Down") },
+
+	-- Navigate between tabs
+	{ key = "LeftArrow", mods = "SUPER|ALT", action = act.ActivateTabRelative(-1) },
+	{ key = "RightArrow", mods = "SUPER|ALT", action = act.ActivateTabRelative(1) },
 }
 
 config.initial_cols = 120
@@ -32,11 +39,11 @@ config.font_size = 19
 config.color_scheme = "Gruvbox Dark (Gogh)"
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = true
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.95
 config.macos_window_background_blur = 10
 
 -- disabling the annoying close confirmation
-config.window_close_confirmation = 'NeverPrompt'
+config.window_close_confirmation = "NeverPrompt"
 
 -- Finally, return the configuration to wezterm:
 return config
