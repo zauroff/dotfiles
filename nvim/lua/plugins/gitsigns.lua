c362b7489756642e610d34275b5e8914c8775f28
DZAUROFF1 <DZAUROFF1@bloomberg.net>
2026-06-09 22:23:58 -0400

    git sturf

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -4,5 +4,21 @@ return {
         opts = {
             current_line_blame = true,
         },
+        keys = {
+            {
+                "<leader>grb",
+                function()
+                    require("gitsigns").change_base("origin/develop", true)
+                end,
+                desc = "Gitsigns base: develop",
+            },
+            {
+                "<leader>grr",
+                function()
+                    require("gitsigns").reset_base(true)
+                end,
+                desc = "Gitsigns base: reset",
+            },
+        },
     },
 }
