5fd09fbfc4f4c753af97deb185849d09daecf9cc
Daniel Zauroff <zauroff@daniels-mbp.mynetworksettings.com>
2026-08-01 01:37:36 -0400

    added constellation

---
+++ nvim/lua/plugins/constellation.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "zauroff/constellation.nvim",
+        cmd = { "Constellation", "Graph" },
+        keys = {
+            { "<leader>cu", "<cmd>Constellation<cr>", desc = "Usage Graph (local)" },
+        },
+        opts = {},
+    },
+}
